ccae1efe97ef161e5f26225fd1abc1c95f18eee5
binghan60 <binghan60@gmail.com>
2024-12-11 17:38:45 +0800

    client:註冊

--- client/src/views/RegisterView.vue
+++ client/src/views/RegisterView.vue
@@ -2,7 +2,7 @@
 export default {
   data() {
     return {
-      account: '',
+      username: '',
       password: '',
       confirmPassword: '',
       showPassword: false,
@@ -16,13 +16,40 @@ export default {
     toggleConfirmPassword() {
       this.showConfirmPassword = !this.showConfirmPassword
     },
-    handleSubmit() {
+    async register() {
+      if (!this.password || !this.password) {
+        alert('請輸入密碼')
+        return
+      }
       if (this.password !== this.confirmPassword) {
         alert('密碼與確認密碼不一致')
         return
       }
-      console.log('Account:', this.account)
-      console.log('Password:', this.password)
+      const { username, password } = this
+      try {
+        const response = await fetch(`http://localhost:3000/user/register`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ username, password }),
+        })
+        if (!response.ok) {
+          throw new Error(`Request failed with status: ${response.status}`)
+        }
+        const registerResponse = await response.json()
+        if (!registerResponse.isSuccess) {
+          alert(registerResponse.message)
+          return
+        }
+        // TODO
+        // 成功的畫面呈現
+        alert(registerResponse.message)
+      } catch (error) {
+        alert('伺服器忙碌中，請稍後再試。')
+        console.error('register', error)
+        throw error
+      }
     },
   },
 }
@@ -34,8 +61,8 @@ export default {
       <h2 class="mb-6 text-2xl font-bold text-center text-gray-800">註冊</h2>
       <form @submit.prevent="handleSubmit">
         <div class="mb-4">
-          <label for="account" class="text-gray-700">帳號</label>
-          <input type="text" id="account" v-model="account" class="w-full h-8 pl-3 mt-1 border-gray-300 rounded-md shadow-sm focus:border-primary-500 focus:border-2 focus:outline-none" placeholder="請輸入帳號" autocomplete="off" />
+          <label for="username" class="text-gray-700">帳號</label>
+          <input type="text" id="username" v-model="username" class="w-full h-8 pl-3 mt-1 border-gray-300 rounded-md shadow-sm focus:border-primary-500 focus:border-2 focus:outline-none" placeholder="請輸入帳號" autocomplete="off" />
         </div>
         <div class="mb-4">
           <label for="password" class="text-gray-700">密碼</label>
@@ -58,7 +85,7 @@ export default {
           </div>
         </div>
         <!-- Submit -->
-        <button type="submit" class="w-full px-4 py-2 text-white rounded-md bg-primary-600 hover:bg-primary-700 focus:ring-2 focus:ring-primary-500 focus:ring-offset-2 focus:outline-none">註冊</button>
+        <button type="submit" class="w-full px-4 py-2 text-white rounded-md bg-primary-600 hover:bg-primary-700 focus:ring-2 focus:ring-primary-500 focus:ring-offset-2 focus:outline-none" @click="register">註冊</button>
         <p class="mt-4 text-sm text-center text-gray-600">已經有帳號？<RouterLink to="/login" class="text-primary-600 hover:underline">登入</RouterLink></p>
       </form>
     </div>
