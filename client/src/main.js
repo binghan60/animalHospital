5b9dd3c0f672a89e980f34869f124e1f43a55126
binghan <binghan60@gmail.com>
2024-12-11 20:31:12 +0800

    client:設定並全域註冊vue-toastification

--- client/src/main.js
+++ client/src/main.js
@@ -1,14 +1,33 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
+import Toast, { useToast } from 'vue-toastification'
+import 'vue-toastification/dist/index.css'
+
+const options = {
+  position: 'bottom-center',
+  timeout: 1500,
+  closeOnClick: true,
+  pauseOnFocusLoss: false,
+  pauseOnHover: false,
+  draggable: true,
+  draggablePercent: 0.25,
+  showCloseButtonOnHover: false,
+  hideProgressBar: false,
+  closeButton: 'button',
+  icon: true,
+  rtl: false,
+  transition: 'Vue-Toastification__fade',
+}
 
 const app = createApp(App)
+const pinia = createPinia()
 
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
+app.use(Toast, options)
 
+app.config.globalProperties.$toast = useToast() // 全域註冊 this.$toast 方法
 app.mount('#app')
