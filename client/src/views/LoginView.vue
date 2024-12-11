23a29f12f9869da9a4fb49fde756133b47bb04b3
binghan60 <binghan60@gmail.com>
2024-12-11 17:38:52 +0800

    client:登入

--- client/src/views/LoginView.vue
+++ client/src/views/LoginView.vue
@@ -2,7 +2,7 @@
 export default {
   data() {
     return {
-      account: '',
+      username: '',
       password: '',
       showPassword: false,
     }
@@ -11,9 +11,35 @@ export default {
     togglePassword() {
       this.showPassword = !this.showPassword
     },
-    handleSubmit() {
-      console.log('account:', this.account)
-      console.log('Password:', this.password)
+    async login() {
+      const { username, password } = this
+      try {
+        const response = await fetch(`http://localhost:3000/user/login`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ username, password }),
+        })
+        if (!response.ok) {
+          throw new Error(`Request failed with status: ${response.status}`)
+        }
+        const loginResponse = await response.json()
+        if (!loginResponse.isSuccess) {
+          alert(loginResponse.message)
+          return
+        }
+        alert(loginResponse.message)
+        console.log(loginResponse)
+        document.cookie = `animalHospitalToken=${loginResponse.token}; expires=${new Date(loginResponse.expired)}`
+        this.$router.push('./animal-list')
+        //TODO
+        //登入成功樣式
+      } catch (error) {
+        alert('伺服器忙碌中，請稍後再試。')
+        console.error('login', error)
+        throw error
+      }
     },
   },
 }
@@ -25,8 +51,8 @@ export default {
       <h2 class="mb-6 text-2xl font-bold text-center text-gray-800">動物健康管理系統</h2>
       <form @submit.prevent="handleSubmit">
         <div class="mb-4">
-          <label for="account" class="text-gray-700">帳號</label>
-          <input type="text" id="account" v-model="account" class="w-full h-8 pl-3 mt-1 border-gray-300 rounded-md shadow-sm focus:border-primary-500 focus:border-2 focus:outline-none" placeholder="請輸入帳號" autocomplete="off" />
+          <label for="username" class="text-gray-700">帳號</label>
+          <input type="text" id="username" v-model="username" class="w-full h-8 pl-3 mt-1 border-gray-300 rounded-md shadow-sm focus:border-primary-500 focus:border-2 focus:outline-none" placeholder="請輸入帳號" autocomplete="off" />
         </div>
         <div class="mb-4">
           <label for="password" class="text-gray-700">密碼</label>
@@ -38,7 +64,7 @@ export default {
             </button>
           </div>
         </div>
-        <button type="submit" class="w-full px-4 py-2 text-white rounded-md bg-primary-600 hover:bg-primary-700 focus:ring-2 focus:ring-primary-500 focus:ring-offset-2 focus:outline-none">登入</button>
+        <button type="submit" class="w-full px-4 py-2 text-white rounded-md bg-primary-600 hover:bg-primary-700 focus:ring-2 focus:ring-primary-500 focus:ring-offset-2 focus:outline-none" @click="login">登入</button>
         <p class="mt-4 text-sm text-center text-gray-600">沒有帳號？<RouterLink to="/register" class="text-primary-600 hover:underline">註冊</RouterLink></p>
       </form>
     </div>
