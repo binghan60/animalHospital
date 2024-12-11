7c5306d9950ff098786953407646429837c05454
binghan60 <binghan60@gmail.com>
2024-12-11 16:29:37 +0800

    server：登入API

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,6 +1,8 @@
 import express from 'express';
-import User from '../model/userModel.js';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import User from '../model/userModel.js';
+import mongoose from 'mongoose';
 
 const router = express.Router();
 
@@ -26,6 +28,32 @@ router.post('/register', async (req, res) => {
         });
         await newUser.save();
         res.status(201).json({ message: '註冊成功' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: '伺服器錯誤' });
+    }
+});
+router.post('/login', async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        if (!username || !password) {
+            return res.status(400).json({ message: '參數不足' });
+        }
+        const user = await User.findOne({ username });
+        if (!user) {
+            return res.status(400).json({ message: '查無帳號' });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: '密碼錯誤' });
+        }
+        const payload = {
+            userId: user._id,
+            username: user.username,
+        };
+        const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '7d' });
+        res.status(200).json({ message: '登入成功', token });
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: '伺服器錯誤' });
