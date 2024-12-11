1cfa3a1a51a9596112a70201e53204f1f8a039ec
binghan60 <binghan60@gmail.com>
2024-12-11 11:22:27 +0800

    新增Tailwinld主色

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+import colors from 'tailwindcss/colors'
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: colors.blue,
+      },
+    },
   },
   plugins: [],
 }
