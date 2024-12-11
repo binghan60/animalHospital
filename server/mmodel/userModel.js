9174e9e68a799375055e0345cb6c1341913648ec
binghan60 <binghan60@gmail.com>
2024-12-11 15:07:15 +0800

    處理資料夾名稱

---
+++ server/mmodel/userModel.js
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        email: {
+            type: String,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        firstName: {
+            type: String,
+            required: true,
+        },
+        lastName: {
+            type: String,
+            required: true,
+        },
+        isActive: {
+            type: Boolean,
+            default: true,
+        },
+        admin: {
+            type: Number,
+            default: 0,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+const User = mongoose.model('User', userSchema);
+
+export default User;
