bbdefd4f5579de05fdffe3eb7b8f314dd9d42c09
binghan60 <binghan60@gmail.com>
2024-12-12 17:01:28 +0800

    client:navbarComponent

--- client/src/components/NavbarComponent.vue
+++ client/src/components/NavbarComponent.vue
@@ -1,33 +1,40 @@
 <script>
 export default {
   data() {
-    return {}
+    return {
+      isMenuOpen: false,
+    }
+  },
+  methods: {
+    toggleMenu() {
+      this.isMenuOpen = !this.isMenuOpen
+    },
+    chagnePage(path) {
+      this.$router.push(path)
+      this.isMenuOpen = false
+    },
   },
 }
 </script>
 
 <template>
-  <nav class="p-4 bg-blue-600">
-    <div class="flex items-center justify-between mx-auto max-w-7xl">
-      <!-- Logo -->
-      <a href="#" class="text-2xl font-bold text-white">MyLogo</a>
-
-      <!-- Navigation Links -->
-      <div class="hidden space-x-6 md:flex">
-        <a href="#" class="text-white hover:text-blue-300">Home</a>
-        <a href="#" class="text-white hover:text-blue-300">About</a>
-        <a href="#" class="text-white hover:text-blue-300">Services</a>
-        <a href="#" class="text-white hover:text-blue-300">Contact</a>
+  <nav class="bg-primary-400">
+    <div class="flex items-center justify-between p-2 mx-auto text-lg text-white max-w-7xl">
+      <a @click="chagnePage('/')">
+        <div class="text-2xl font-bold cursor-pointer">
+          LOGO
+          <!-- <img class="w-auto h-full" src="/img/logo.png" alt="Logo" /> -->
+        </div>
+      </a>
+      <div class="lg:hidden" @click="toggleMenu">
+        <span class="text-2xl">☰</span>
       </div>
-
-      <!-- Mobile Menu Button -->
-      <div class="md:hidden">
-        <button class="text-white">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-6 h-6">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
-          </svg>
-        </button>
-      </div>
+      <ul :class="['lg:flex lg:space-x-6 lg:space-y-0 absolute lg:static top-12 right-0 w-full text-center lg:w-auto transition-all duration-500 ease-out lg:max-h-full max-h-0 overflow-hidden', { 'max-h-72': isMenuOpen }]">
+        <li @click="chagnePage('/styleGuild')" class="relative z-50 flex items-center justify-center h-12 cursor-pointer bg-primary-400">展示中心</li>
+        <li @click="chagnePage('/productlist')" class="relative z-50 flex items-center justify-center h-12 cursor-pointer bg-primary-400">商品列表</li>
+        <li @click="chagnePage('/cart')" class="relative z-50 flex items-center justify-center h-12 cursor-pointer bg-primary-400">購物車</li>
+        <li @click="chagnePage('/member')" class="relative z-50 flex items-center justify-center h-12 cursor-pointer bg-primary-400">會員中心</li>
+      </ul>
     </div>
   </nav>
 </template>
