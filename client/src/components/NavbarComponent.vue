0a81c5198545074bd6e5baa62d486ec14d7f0adf
binghan60 <binghan60@gmail.com>
2024-12-12 16:29:04 +0800

    client:新增navarcomponent

---
+++ client/src/components/NavbarComponent.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+  data() {
+    return {}
+  },
+}
+</script>
+
+<template>
+  <nav class="p-4 bg-blue-600">
+    <div class="flex items-center justify-between mx-auto max-w-7xl">
+      <!-- Logo -->
+      <a href="#" class="text-2xl font-bold text-white">MyLogo</a>
+
+      <!-- Navigation Links -->
+      <div class="hidden space-x-6 md:flex">
+        <a href="#" class="text-white hover:text-blue-300">Home</a>
+        <a href="#" class="text-white hover:text-blue-300">About</a>
+        <a href="#" class="text-white hover:text-blue-300">Services</a>
+        <a href="#" class="text-white hover:text-blue-300">Contact</a>
+      </div>
+
+      <!-- Mobile Menu Button -->
+      <div class="md:hidden">
+        <button class="text-white">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
+          </svg>
+        </button>
+      </div>
+    </div>
+  </nav>
+</template>
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,4 +1,16 @@
-<script></script>
+<script>
+import NavbarComponent from '@/components/NavbarComponent.vue'
+
+export default {
+  data() {
+    return {}
+  },
+  components: {
+    NavbarComponent, // 正確的組件名稱
+  },
+}
+</script>
+
 <template>
-  <h1></h1>
+  <NavbarComponent></NavbarComponent>
 </template>
