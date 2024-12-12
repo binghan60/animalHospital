2975f8243e327915a7de162b81adcf60dfd3a155
binghan60 <binghan60@gmail.com>
2024-12-12 17:01:14 +0800

    client:調整mainjs路徑

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,8 @@
-import './assets/main.css'
+import '@/assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import App from './App.vue'
-import router from './router'
+import App from '@/App.vue'
+import router from '@/router'
 import Toast, { useToast } from 'vue-toastification'
 import 'vue-toastification/dist/index.css'
 
