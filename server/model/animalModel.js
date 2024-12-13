91d32ef80ede33ca6ad6f2d93ee636acf8237cc7
binghan60 <binghan60@gmail.com>
2024-12-13 18:24:16 +0800

    server:建立動物Schema

---
+++ server/model/animalModel.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+const animalSchema = new mongoose.Schema(
+    {
+        userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true, index: true },
+        name: { type: String, required: true },
+        gender: { type: String, enum: ['male', 'female', 'other'] },
+        weight: { type: Number },
+        birthday: { type: Date },
+        sterilized: { type: Boolean },
+        breed: { type: String },
+        bloodType: { type: String, enum: ['A', 'B', 'AB', 'O'] },
+        type: { type: String },
+        insulinBrand: { type: String },
+        admissionDate: { type: Date },
+    },
+    { timestamp: true }
+);
+const Animal = mongoose.model('Animal', animalSchema);
+export default Animal;
