471a0ab1dd96faa4c655a09401fbfe43947a9919
binghan <binghan60@gmail.com>
2024-12-14 21:56:55 +0800

    server:getAnimalAPI

--- server/routes/animalRouter.js
+++ server/routes/animalRouter.js
@@ -1,12 +1,26 @@
 import express from 'express';
 import Animal from '../models/animalModel.js';
+import mongoose from 'mongoose';
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
     res.send({ message: '動物路由' });
 });
-
+router.get('/:userId', async (req, res) => {
+    try {
+        const userId = req.params.userId;
+        const animal = await Animal.find({ userId });
+        if (animal.length > 0) {
+            res.send(animal);
+        } else {
+            res.status(404).send({ message: '找不到動物' });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: '伺服器錯誤' });
+    }
+});
 router.post('/create', async (req, res) => {
     const { userId, name, gender, weight, birthday, sterilized, breed, bloodType, type, insulinBrand, admissionDate } = req.body;
     try {
@@ -26,14 +40,7 @@ router.post('/create', async (req, res) => {
         const animal = await newAnimal.save(); //保存至資料庫
         res.send({ animal });
     } catch (error) {
-        // 捕獲 Mongoose validation 錯誤
-        console.error(error);
-        if (error.name === 'ValidationError') {
-            // 如果是驗證錯誤，返回詳細錯誤信息
-            return res.status(400).json({ message: 'Validation Error', errors: error.errors });
-        }
-        // 其他錯誤，返回 500 錯誤
-        res.status(500).json({ message: 'Server Error' });
+        res.status(500).json({ message: error.errors });
     }
 });
 router.get('/', async (req, res) => {});
