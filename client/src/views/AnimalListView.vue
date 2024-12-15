fa797f34e6b92d749e0d591ba38a92a8bf3f1f36
binghan <binghan60@gmail.com>
2024-12-15 13:09:03 +0800

    client:串接API

--- client/src/views/AnimalListView.vue
+++ client/src/views/AnimalListView.vue
@@ -1,4 +1,7 @@
 <script>
+import apiStore from '@/stores/api'
+import authStore from '@/stores/auth'
+import { mapState } from 'pinia'
 export default {
   data() {
     return {
@@ -13,22 +16,101 @@ export default {
         bloodType: '',
         type: ' ',
         insulinBrand: '',
-        admissionDate: '',
+        admissionDate: new Date('2024-12-08').toISOString().slice(0, 10),
       },
       iscreateFormOpen: false,
+      otherField: true,
+      searchKeyword: '',
     }
   },
   methods: {
-    openDatePicker() {
-      // 觸發日期選擇器顯示
+    async creatNewAnimal() {
+      try {
+        const response = await fetch(`${this.apipath}/animal/create`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            userId: this.user._id,
+            name: this.createForm.name,
+            gender: this.createForm.gender,
+            weight: [{ date: new Date('2024-12-08').toISOString().slice(0, 10), value: this.createForm.weight }],
+            birthday: this.createForm.birthday,
+            sterilized: this.createForm.sterilized,
+            breed: this.createForm.breed,
+            bloodType: this.createForm.bloodType,
+            type: this.createForm.type,
+            insulinBrand: this.createForm.insulinBrand,
+            admissionDate: this.createForm.admissionDate,
+          }),
+        })
+        if (!response.ok) {
+          this.$toast.error(response.message)
+          return
+        }
+        this.$toast.success('新增成功')
+        await this.getUserAnimal()
+        this.iscreateFormOpen = false
+      } catch (error) {
+        this.$toast.error('伺服器忙碌中，請稍後再試。')
+        throw error
+      }
+    },
+    async getUserAnimal() {
+      try {
+        const response = await fetch(`${this.apipath}/animal/${this.user._id}?searchKeyword=${this.searchKeyword}`, {
+          method: 'GET',
+        })
+        const animalList = await response.json()
+        if (!response.ok) {
+          this.$toast.error(animalList.message)
+          return
+        }
+        this.animalList = animalList
+      } catch (error) {
+        this.$toast.error('伺服器忙碌中，請稍後再試。')
+        throw error
+      }
+    },
+    convertBirthdayToAge(dateString) {
+      const today = new Date()
+      const birth = new Date(dateString)
+      let years = today.getFullYear() - birth.getFullYear()
+      let months = today.getMonth() - birth.getMonth()
+      if (months < 0) {
+        years--
+        months += 12
+      }
+      return { years, months }
+    },
+    async handleSearchBlur() {
+      if (this.searchKeyword.trim() === '') {
+        return
+      }
+      await this.getUserAnimal()
+    },
+    async handleViewAll() {
+      this.searchKeyword = ''
+      await this.getUserAnimal()
     },
   },
-  mounted() {},
+  computed: {
+    ...mapState(apiStore, ['apipath']),
+    ...mapState(authStore, ['user']),
+  },
+  async mounted() {
+    await this.getUserAnimal()
+    // TODO 表單驗證
+    // 照片上傳
+    // API按鈕防呆
+    // 出院住院紀錄
+  },
 }
 </script>
 
 <template>
-  <div class="p-4 mx-auto lg:p-2 max-w-7xl">
+  <div>
     <div class="relative flex flex-col w-full h-full text-gray-700 bg-white shadow-md rounded-xl bg-clip-border">
       <div class="relative mx-4 mt-4 overflow-hidden text-gray-700 bg-white rounded-none bg-clip-border">
         <div class="flex items-center justify-between gap-8 mb-8">
@@ -37,7 +119,7 @@ export default {
             <p class="block mt-1 font-sans text-base antialiased font-normal leading-relaxed text-gray-700"></p>
           </div>
           <div class="flex flex-col gap-2 shrink-0 sm:flex-row">
-            <button class="select-none rounded-lg border border-gray-900 py-2 px-4 text-center align-middle font-sans text-xs font-bold uppercase text-gray-900 transition-all hover:opacity-75 focus:ring focus:ring-gray-300 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" type="button">view all</button>
+            <button class="select-none rounded-lg border border-gray-900 py-2 px-4 text-center align-middle font-sans text-xs font-bold uppercase text-gray-900 transition-all hover:opacity-75 focus:ring focus:ring-gray-300 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" type="button" @click="handleViewAll">view all</button>
             <button @click="iscreateFormOpen = true" class="flex select-none items-center gap-3 rounded-lg bg-primary-600 py-2 px-4 text-center text-xs font-bold text-white shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-primary-600/20 focus:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" type="button">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" stroke-width="2" class="w-4 h-4">
                 <path d="M6.25 6.375a4.125 4.125 0 118.25 0 4.125 4.125 0 01-8.25 0zM3.25 19.125a7.125 7.125 0 0114.25 0v.003l-.001.119a.75.75 0 01-.363.63 13.067 13.067 0 01-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 01-.364-.63l-.001-.122zM19.75 7.5a.75.75 0 00-1.5 0v2.25H16a.75.75 0 000 1.5h2.25v2.25a.75.75 0 001.5 0v-2.25H22a.75.75 0 000-1.5h-2.25V7.5z"></path>
@@ -58,7 +140,7 @@ export default {
                   <div class="z-10">&nbsp;&nbsp;住院&nbsp;&nbsp;</div>
                 </li>
                 <li role="tab" class="relative flex items-center justify-center w-full h-full px-2 py-1 text-center cursor-pointer select-none text-primary-900">
-                  <div class="z-10">&nbsp;&nbsp;糖尿病&nbsp;&nbsp;</div>
+                  <div class="z-10">&nbsp;&nbsp;出院&nbsp;&nbsp;</div>
                 </li>
               </ul>
             </nav>
@@ -70,11 +152,8 @@ export default {
                   <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"></path>
                 </svg>
               </div>
-              <input class="peer h-full w-full rounded-[7px] border border-primary-200 border-t-transparent bg-transparent px-3 py-2.5 !pr-9 font-sans text-sm font-normal text-primary-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-primary-200 placeholder-shown:border-t-primary-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-primary-50" placeholder=" " />
-              <label
-                class="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none !overflow-visible truncate text-[11px] font-normal leading-tight text-gray-500 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-primary-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-primary-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-primary-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-gray-900 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-gray-900 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-primary-500"
-                >搜尋
-              </label>
+              <input class="peer h-full w-full rounded-[7px] border border-primary-200 border-t-transparent bg-transparent px-3 py-2.5 !pr-9 font-sans text-sm font-normal text-primary-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-primary-200 placeholder-shown:border-t-primary-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-primary-50" placeholder=" " v-model="searchKeyword" @blur="handleSearchBlur" />
+              <label class="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none !overflow-visible truncate text-[11px] font-normal leading-tight text-gray-500 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-primary-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-primary-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-primary-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-gray-900 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-gray-900 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-primary-500">搜尋 </label>
             </div>
           </div>
         </div>
@@ -137,40 +216,40 @@ export default {
             </tr>
           </thead>
           <tbody>
-            <tr class="hover:bg-primary-100">
+            <tr class="hover:bg-primary-100" v-for="animal in animalList" :key="animal._id" @click="this.$router.push(`/animal/${animal._id}`)">
               <td class="p-4 border-b border-primary-50">
                 <div class="flex items-center gap-3">
                   <img src="/image/1.jpg" alt="John Michael" class="relative inline-block h-9 w-9 !rounded-full object-cover object-center" />
                   <div class="flex flex-col">
-                    <p class="block font-sans text-sm antialiased font-normal leading-normal text-primary-900">牛奶</p>
-                    <p class="block font-sans text-sm antialiased font-normal leading-normal text-primary-900 opacity-70">貓咪 12歲</p>
+                    <p class="block font-sans text-sm antialiased font-bold leading-normal text-primary-900">{{ animal.name }}</p>
+                    <p class="block font-sans text-sm antialiased font-normal leading-normal text-primary-900 opacity-70">{{ animal.type == 'dog' ? '狗狗' : '貓貓' }} {{ convertBirthdayToAge(animal.birthday).years }}歲 {{ convertBirthdayToAge(animal.birthday).months > 0 ? convertBirthdayToAge(animal.birthday).months + '個月' : '' }}</p>
                   </div>
                 </div>
               </td>
               <td class="p-4 border-b border-primary-50">
                 <div class="flex flex-col">
-                  <p class="block font-sans text-sm antialiased font-normal leading-normal text-primary-900">5.5 公斤</p>
+                  <p class="block font-sans text-sm antialiased font-normal leading-normal text-primary-900">{{ animal.weight[animal.weight.length - 1].value }} 公斤</p>
                 </div>
               </td>
               <td class="p-4 border-b border-primary-50">
                 <div class="flex flex-col">
-                  <p class="block font-sans text-sm antialiased font-normal leading-normal text-primary-900">A 型</p>
+                  <p class="block font-sans text-sm antialiased font-normal leading-normal text-primary-900">{{ animal.bloodType }} 型</p>
                 </div>
               </td>
               <td class="p-4 border-b border-primary-50">
                 <div class="flex flex-col">
-                  <p class="block font-sans text-sm antialiased font-normal leading-normal text-primary-900">米克斯</p>
+                  <p class="block font-sans text-sm antialiased font-normal leading-normal text-primary-900">{{ animal.breed }}</p>
                 </div>
               </td>
               <td class="p-4 border-b border-primary-50">
                 <div class="w-max">
-                  <div class="relative grid items-center px-2 py-1 font-sans text-xs font-bold text-green-900 uppercase rounded-md select-none whitespace-nowrap bg-green-500/20">
-                    <span>是</span>
+                  <div :class="[animal.sterilized ? 'text-green-900' : 'text-red-900', animal.sterilized ? 'bg-green-500/20' : 'bg-red-500/20']" class="relative grid items-center px-2 py-1 font-sans text-xs font-bold uppercase rounded-md select-none whitespace-nowrap">
+                    <span>{{ animal.sterilized ? '是' : '否' }}</span>
                   </div>
                 </div>
               </td>
               <td class="p-4 border-b border-primary-50">
-                <p class="block font-sans text-sm antialiased font-normal leading-normal text-primary-900">2024/12/13</p>
+                <p class="block font-sans text-sm antialiased font-normal leading-normal text-primary-900">{{ new Date(animal.admissionDate).toISOString().slice(0, 10) }}</p>
               </td>
               <td class="p-4 border-b border-primary-50">
                 <button class="relative h-10 max-h-[40px] w-10 max-w-[40px] select-none rounded-lg text-center align-middle font-sans text-xs font-medium uppercase text-gray-900 transition-all hover:bg-gray-900/10 active:bg-gray-900/20 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" type="button">
@@ -213,7 +292,7 @@ export default {
             </div>
             <div class="flex items-center">
               <label for="birthday" class="w-1/3 text-left text-primary-700">生日</label>
-              <input id="birthday" ref="birthday" type="date" class="block w-full p-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-primary-300" v-model="createForm.birthday" @click="$refs.admissionDate.showPicker()" />
+              <input id="birthday" ref="birthday" type="date" class="block w-full p-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-primary-300" @focus="$refs.birthday.showPicker()" v-model="createForm.birthday" />
             </div>
             <div class="flex items-center">
               <label class="w-1/3 text-left text-primary-700">是否結紮</label>
@@ -263,28 +342,28 @@ export default {
                   <span class="px-2 py-2 transition-all rounded-lg shadow-md cursor-pointer lg:px-2 text-primary-800 bg-primary-100 peer-checked:bg-primary-500 peer-checked:text-white"><i class="fa-solid fa-dog"></i> 狗狗 </span>
                 </label>
                 <label class="min-w-[48px] rounded-l">
-                  <input type="radio" name="radioButton" value="" class="hidden peer" @click="createForm.type = ''" />
+                  <input type="radio" name="radioButton" value="other" class="hidden peer" v-model="createForm.type" />
                   <span class="px-2 py-2 transition-all rounded-lg shadow-md cursor-pointer lg:px-2 text-primary-800 bg-primary-100 peer-checked:bg-primary-500 peer-checked:text-white">其他 </span>
                 </label>
-                <label class="rounded-l" v-if="createForm.type == ''">
-                  <input type="text" placeholder="其他" class="w-full p-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-primary-300" v-model="createForm.type" />
-                </label>
+                <!-- <label class="rounded-l">
+                  <input type="text" placeholder="其他" class="w-full p-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-primary-300" />
+                </label> -->
               </div>
             </div>
 
             <div class="flex items-center">
               <label for="insulinBrand" class="w-1/3 text-left text-primary-700">胰島素品牌</label>
-              <input id="insulinBrand" type="text" placeholder="胰島素品牌" class="block w-full p-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-primary-300" />
+              <input id="insulinBrand" type="text" placeholder="胰島素品牌" class="block w-full p-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-primary-300" v-model="createForm.insulinBrand" />
             </div>
             <div class="flex items-center">
               <label for="admissionDate" class="w-1/3 text-left text-primary-700">入院日期</label>
-              <input ref="admissionDate" type="date" class="block w-full p-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-primary-300" @click="$refs.admissionDate.showPicker()" />
+              <input ref="admissionDate" type="date" class="block w-full p-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-primary-300" @focus="$refs.admissionDate.showPicker()" v-model="createForm.admissionDate" />
             </div>
           </div>
         </div>
         <div class="flex justify-between">
           <button class="w-1/3 px-6 py-2 text-gray-700 transition-all bg-gray-300 rounded-lg shadow-md hover:bg-gray-400" @click="iscreateFormOpen = false">取消</button>
-          <button class="w-1/3 px-6 py-2 text-white transition-all rounded-lg shadow-md bg-primary-500 hover:bg-primary-400">確定</button>
+          <button class="w-1/3 px-6 py-2 text-white transition-all rounded-lg shadow-md bg-primary-500 hover:bg-primary-400" @click="creatNewAnimal">確定</button>
         </div>
       </div>
     </div>
