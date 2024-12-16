2c9f95b0547d23004abd6de481a76ebf2ef92a38
binghan60 <binghan60@gmail.com>
2024-12-16 15:49:13 +0800

    clinet:除錯用navbar

--- client/src/components/NavbarComponent.vue
+++ client/src/components/NavbarComponent.vue
@@ -32,7 +32,7 @@ export default {
 <template>
   <nav class="mb-10 bg-primary-400">
     <div class="flex items-center justify-between p-2 mx-auto text-lg text-white max-w-7xl">
-      <a @click="chagnePage('/animallist')">
+      <a @click="chagnePage('/')">
         <div class="text-2xl font-bold cursor-pointer">
           LOGO
           <!-- <img class="w-auto h-full" src="/img/logo.png" alt="Logo" /> -->
@@ -40,6 +40,7 @@ export default {
       </a>
       <div class="lg:hidden" @click="toggleMenu"><span class="text-2xl">☰</span></div>
       <ul :class="['lg:flex lg:space-x-6 lg:space-y-0 absolute lg:static top-12 right-0 w-full text-center lg:w-auto transition-all duration-500 ease-out lg:max-h-full max-h-0 overflow-hidden', { 'max-h-72': isMenuOpen }]">
+        <li @click="chagnePage('/animallist')" class="relative z-10 flex items-center justify-center h-10 cursor-pointer bg-primary-400">動物列表</li>
         <li class="relative z-10 flex items-center justify-center h-10 bg-primary-400">{{ user.username + (user.title ? user.title : '') }}</li>
         <li @click="logOut" class="relative z-10 flex items-center justify-center h-10 cursor-pointer bg-primary-400">登出</li>
       </ul>
