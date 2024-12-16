7b7f46eced20520f91eb2545b0beaf3c12066bcc
binghan60 <binghan60@gmail.com>
2024-12-16 18:12:11 +0800

    server:新增Model

---
+++ server/models/bloodSugarModel.js
@@ -0,0 +1,35 @@
+﻿import mongoose from 'mongoose';
+
+const bloodSugarSchema = new mongoose.Schema(
+    {
+        animalId: { type: mongoose.Schema.Types.ObjectId, ref: 'Animal', required: true, index: true },
+        date: {
+            type: Date,
+            required: true,
+        },
+        morning: {
+            bloodSugar: {
+                type: Number,
+            },
+            insulin: {
+                type: Number,
+            },
+        },
+        evening: {
+            bloodSugar: {
+                type: Number,
+            },
+            insulin: {
+                type: Number,
+            },
+        },
+        notes: {
+            type: String,
+            default: '',
+        },
+    },
+    { timestamps: true }
+);
+
+const BloodSugarSchema = mongoose.model('DiaryBloodRecord', bloodSugarSchema);
+export default BloodSugarSchema;
