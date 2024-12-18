fdc538f3eefdec2e6eb15d5e7e98be9cb5a5b4df
binghan <binghan60@gmail.com>
2024-12-18 22:53:54 +0800

    server:新增token登入

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -34,34 +34,54 @@ router.post('/register', async (req, res) => {
 });
 router.post('/login', async (req, res) => {
     try {
-        const { username, password } = req.body;
-        if (!username || !password) {
-            return res.status(400).json({ message: '參數不足' });
+        const authorization = req.headers.authorization;
+        const token = authorization?.slice(7, authorization.length);
+        if (token) {
+            jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
+                if (err) {
+                    return res.status(403).json({ message: 'Token is not valid' });
+                }
+                const user = await User.findById(decoded.userId);
+                if (!user) {
+                    return res.status(400).json({ message: 'User not found' });
+                }
+                if (!user.isActive) {
+                    return res.status(400).json({ message: '帳號已停用' });
+                }
+                const response = user.toObject();
+                delete response.password;
+                return res.status(200).json({ ...response, message: '登入成功', token });
+            });
+        } else {
+            // 如果沒有token，就用帳號密碼
+            const { username, password } = req.body;
+            if (!username || !password) {
+                return res.status(400).json({ message: '參數不足' });
+            }
+            const user = await User.findOne({ username });
+            if (!user) {
+                return res.status(400).send({ message: '查無帳號' });
+            }
+            const isMatch = await bcrypt.compare(password, user.password);
+            if (!isMatch) {
+                return res.status(400).json({ message: '密碼錯誤' });
+            }
+            if (!user.isActive) {
+                return res.status(400).json({ message: '帳號已停用' });
+            }
+            const payload = {
+                userId: user._id,
+                username: user.username,
+            };
+            const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '7d' });
+            const expired = Date.now() + 7 * 24 * 60 * 60 * 1000;
+            const response = user.toObject();
+            delete response.password;
+            return res.status(200).json({ ...response, message: '登入成功', token, expired });
         }
-        const user = await User.findOne({ username });
-        if (!user) {
-            return res.status(400).send({ message: '查無帳號' });
-        }
-        const isMatch = await bcrypt.compare(password, user.password);
-        if (!isMatch) {
-            return res.status(400).json({ message: '密碼錯誤' });
-        }
-        if (!user.isActive) {
-            return res.status(400).json({ message: '帳號已停用' });
-        }
-        console.log(user);
-        const payload = {
-            userId: user._id,
-            username: user.username,
-        };
-        const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '7d' });
-        const expired = Date.now() + 7 * 24 * 60 * 60 * 1000;
-        const response = user.toObject();
-        delete response.password;
-        res.status(200).json({ ...response, message: '登入成功', token, expired });
     } catch (error) {
         console.error(error);
-        res.status(500).json({ message: '伺服器錯誤' });
+        return res.status(500).json({ message: '伺服器錯誤' });
     }
 });
 
