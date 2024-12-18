0eb3966163c12976d3a9756cb8726b2753eb4cea
binghan60 <binghan60@gmail.com>
2024-12-18 17:44:28 +0800

    client:統一外觀風格

--- client/src/views/RegisterView.vue
+++ client/src/views/RegisterView.vue
@@ -60,37 +60,37 @@ export default {
 </script>
 
 <template>
-  <div class="flex items-center justify-center min-h-screen p-6 bg-primary-100">
-    <div class="w-full max-w-sm min-w-[330px] p-6 bg-white rounded-lg shadow-lg lg:p-8">
-      <h2 class="mb-6 text-2xl font-bold text-center text-gray-800">註冊</h2>
+  <div class="flex items-center justify-center min-h-screen p-6">
+    <div class="w-full h-full max-w-sm min-w-[330px] p-6 lg:bg-white lg:rounded-lg lg:shadow-lg lg:p-8">
+      <h2 class="mb-6 text-2xl font-bold text-center text-primary-900">註冊</h2>
       <form @submit.prevent="handleSubmit">
         <div class="mb-4">
-          <label for="username" class="text-gray-700">帳號</label>
-          <input type="text" id="username" v-model="username" class="w-full h-8 pl-3 mt-1 border-gray-300 rounded-md shadow-sm focus:border-primary-500 focus:border-2 focus:outline-none" placeholder="請輸入帳號" autocomplete="off" />
+          <label for="username" class="text-primary-900">帳號</label>
+          <input type="text" id="username" v-model="username" class="w-full h-8 pl-3 mt-2 rounded-md shadow-sm text-primary-900 outline-1 outline-primary-100 focus:outline-2 focus:outline-primary-400 focus:outline-none" placeholder="請輸入帳號" autocomplete="off" />
         </div>
         <div class="mb-4">
-          <label for="password" class="text-gray-700">密碼</label>
-          <div class="relative flex items-center mt-1">
-            <input :type="showPassword ? 'text' : 'password'" id="password" v-model="password" class="w-full h-8 pl-3 pr-10 border-gray-300 rounded-md shadow-sm focus:border-primary-500 focus:border-2 focus:outline-none" placeholder="••••••••" />
-            <button type="button" tabindex="-1" class="absolute inset-y-0 flex items-center justify-center text-gray-500 right-3 hover:text-primary-600" @click="togglePassword">
+          <label for="password" class="text-primary-900">密碼</label>
+          <div class="relative flex items-center mt-2">
+            <input :type="showPassword ? 'text' : 'password'" id="password" v-model="password" class="w-full h-8 pl-3 rounded-md shadow-sm text-primary-900 outline-1 outline-primary-100 focus:outline-2 focus:outline-primary-400 focus:outline-none" placeholder="••••••••" />
+            <button type="button" tabindex="-1" class="absolute flex items-center justify-center h-full text-gray-500 right-3 hover:text-primary-600" @click="togglePassword">
               <i v-if="!showPassword" class="fa-solid fa-eye-slash"></i>
-              <i v-else class="fa-solid fa-eye"></i>
+              <i v-else class="fa-solid fa-eye text-primary-600"></i>
             </button>
           </div>
         </div>
         <div class="mb-4">
-          <label for="confirmPassword" class="text-gray-700">確認密碼</label>
-          <div class="relative flex items-center mt-1">
-            <input :type="showConfirmPassword ? 'text' : 'password'" id="confirmPassword" v-model="confirmPassword" class="w-full h-8 pl-3 pr-10 border-gray-300 rounded-md shadow-sm focus:border-primary-500 focus:border-2 focus:outline-none" placeholder="••••••••" />
-            <button type="button" tabindex="-1" class="absolute inset-y-0 flex items-center justify-center text-gray-500 right-3 hover:text-primary-600" @click="toggleConfirmPassword">
+          <label for="confirmPassword" class="text-primary-900">確認密碼</label>
+          <div class="relative flex items-center mt-2">
+            <input :type="showConfirmPassword ? 'text' : 'password'" id="confirmPassword" v-model="confirmPassword" class="w-full h-8 pl-3 rounded-md shadow-sm text-primary-900 outline-1 outline-primary-100 focus:outline-2 focus:outline-primary-400 focus:outline-none" placeholder="••••••••" />
+            <button type="button" tabindex="-1" class="absolute flex items-center justify-center h-full text-gray-500 right-3 hover:text-primary-600" @click="toggleConfirmPassword">
               <i v-if="!showConfirmPassword" class="fa-solid fa-eye-slash"></i>
-              <i v-else class="fa-solid fa-eye"></i>
+              <i v-else class="fa-solid fa-eye text-primary-600"></i>
             </button>
           </div>
         </div>
         <!-- Submit -->
-        <button type="submit" class="w-full px-4 py-2 text-white rounded-md bg-primary-600 hover:bg-primary-700 focus:ring-2 focus:ring-primary-500 focus:ring-offset-2 focus:outline-none" @click="register">註冊</button>
-        <p class="mt-4 text-sm text-center text-gray-600">已經有帳號？<RouterLink to="/login" class="text-primary-600 hover:underline">登入</RouterLink></p>
+        <button type="submit" class="w-full px-4 py-2 text-white rounded-md bg-primary-600 hover:bg-primary-700 outline-1 focus:outline-2 focus:outline-primary-500 focus:outline-offset-2 focus:outline-none" @click="register">註冊</button>
+        <p class="mt-4 text-sm text-center text-primary-900">已經有帳號？<RouterLink to="/login" class="text-primary-600 hover:underline">登入</RouterLink></p>
       </form>
     </div>
   </div>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -11,3 +11,8 @@ export default {
   },
   plugins: [],
 }
+//背景 50
+//focus ring 400
+//按鈕600 :hover 700
+//取消紐gray-300 :hover400
+//文字900
