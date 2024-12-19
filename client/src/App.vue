ad1260334e1fd400c16e1f85756b6d16c55e24c5
binghan60 <binghan60@gmail.com>
2024-12-19 14:29:47 +0800

    client:改用axios

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,8 +1,8 @@
 <script>
 import { RouterView } from 'vue-router'
 import authStore from '@/stores/auth'
-import apiStore from '@/stores/api'
 import { mapState, mapActions } from 'pinia'
+import axios from 'axios'
 export default {
   components: { RouterView },
   data() {
@@ -10,37 +10,34 @@ export default {
   },
   methods: {
     async autoLogin() {
-      try {
-        const cookies = document.cookie.split('; ')
-        const tokenCookie = cookies.find(row => row.startsWith('animalHospitalToken='))
-        const token = tokenCookie ? tokenCookie.split('=')[1] : null
-        this.setToken(token)
-        const response = await fetch(`${this.apipath}/user/login`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${token}`,
+      const cookies = document.cookie.split('; ')
+      const tokenCookie = cookies.find(row => row.startsWith('animalHospitalToken='))
+      const token = tokenCookie ? tokenCookie.split('=')[1] : null
+      if (token) {
+        const { data } = await axios.post(
+          `${import.meta.env.VITE_API_PATH}/user/login`,
+          {},
+          {
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: `Bearer ${token}`,
+            },
           },
-        })
-        if (response.ok) {
-          const userData = await response.json()
-          this.auth(userData) // 要補資料
-          const redirectPath = this.redirectPath || '/animallist'
-          this.clearRedirectPath()
-          this.$router.push(redirectPath)
-        }
-      } catch (error) {
-        console.error('自動登入失敗', error)
+        )
+        this.auth(data)
+        const redirectPath = this.redirectPath || '/animallist'
+        this.$router.push(redirectPath)
+        this.clearRedirectPath()
+        axios.defaults.headers.common.Authorization = `Bearer ${token}`
       }
     },
     ...mapActions(authStore, ['auth', 'clearRedirectPath', 'setToken']),
   },
   computed: {
     ...mapState(authStore, ['redirectPath']),
-    ...mapState(apiStore, ['apipath']),
   },
-  mounted() {
-    this.autoLogin()
+  async mounted() {
+    await this.autoLogin()
     //axios自動帶token
     // const token = document.cookie.replace(/(?:(?:^|.*;\s*)Token\s*\=\s*([^;]*).*$)|^.*$/, "$1");
     // axios.defaults.headers.common.Authorization = token;
