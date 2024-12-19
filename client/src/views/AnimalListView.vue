c492896a3c746180c54c6206715e691bef4ec4ca
binghan60 <binghan60@gmail.com>
2024-12-19 17:07:05 +0800

    client:新增編輯動物功能

--- client/src/views/AnimalListView.vue
+++ client/src/views/AnimalListView.vue
@@ -14,13 +14,28 @@ export default {
         sterilized: false,
         breed: '',
         bloodType: '',
-        type: ' ',
+        type: '',
         insulinBrand: '',
         admissionDate: new Date().toISOString().slice(0, 10),
       },
-      iscreateFormOpen: false,
+      editForm: {
+        animalId: '',
+        name: '',
+        gender: 'male',
+        weight: '',
+        birthday: '',
+        sterilized: false,
+        breed: '',
+        bloodType: '',
+        type: '',
+        insulinBrand: '',
+        admissionDate: new Date().toISOString().slice(0, 10),
+      },
+      createFormToggle: false,
+      editFormToggle: false,
       otherField: true,
       searchKeyword: '',
+      filterType: 'all',
     }
   },
   methods: {
@@ -46,10 +61,48 @@ export default {
         })
         this.$toast.success('新增成功')
         await this.getUserAnimal()
-        this.iscreateFormOpen = false
+        this.createFormToggle = false
       } catch (error) {
         this.$toast.error(error.response.data.message)
       }
+    },
+    async editAnimal() {
+      const payload = this.editForm
+      try {
+        await axios.put(`${import.meta.env.VITE_API_PATH}/animal/edit`, payload, {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        })
+        this.$toast.success('修改成功')
+        this.editFormToggle = false
+        await this.getUserAnimal()
+      } catch (error) {
+        this.$toast.error(error.response.data.message)
+      }
+    },
+    editToggle(event, animalId) {
+      event.stopPropagation()
+      const animal = this.animalList.find(x => x._id === animalId)
+      if (!animal) {
+        this.$toast.error('發生錯誤')
+        return
+      }
+      const { _id, name, gender, weight, birthday, sterilized, breed, bloodType, type, insulinBrand, admissionDate } = animal
+      this.editForm = {
+        animalId: _id,
+        name,
+        gender,
+        weight: weight?.[0]?.value || '',
+        birthday: new Date(birthday).toISOString().split('T')[0],
+        sterilized,
+        breed,
+        bloodType,
+        type,
+        insulinBrand,
+        admissionDate: new Date(admissionDate).toISOString().slice(0, 10),
+      }
+      this.editFormToggle = true
     },
     async getUserAnimal() {
       try {
@@ -84,9 +137,24 @@ export default {
       this.searchKeyword = ''
       await this.getUserAnimal()
     },
+    animalIcon(type) {
+      if (type == 'dog') {
+        return 'fa-dog'
+      } else if (type == 'cat') {
+        return 'fa-cat'
+      } else if (type == 'other') {
+        return 'fa-question'
+      }
+    },
   },
   computed: {
     ...mapState(authStore, ['user', 'token']),
+    showData() {
+      if (this.filterType == 'all') {
+        return this.animalList
+      }
+      return this.animalList.filter(x => x.type === this.filterType)
+    },
   },
   async mounted() {
     await this.getUserAnimal()
@@ -108,8 +176,8 @@ export default {
             <p class="block mt-1 font-sans text-base antialiased font-normal leading-relaxed text-gray-700"></p>
           </div>
           <div class="flex flex-col gap-2 shrink-0 sm:flex-row">
-            <button class="select-none rounded-lg border border-gray-900 py-2 px-4 text-center align-middle font-sans text-xs font-bold uppercase text-gray-900 transition-all hover:opacity-75 focus:ring focus:ring-gray-300 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" type="button" @click="handleViewAll">view all</button>
-            <button @click="iscreateFormOpen = true" class="flex select-none items-center gap-3 rounded-lg bg-primary-600 py-2 px-4 text-center text-xs font-bold text-white shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-primary-600/20 focus:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" type="button">
+            <button class="select-none rounded-lg border border-gray-900 py-2 px-4 text-center align-middle font-sans text-xs font-bold uppercase text-gray-900 transition-all hover:opacity-75 focus:ring focus:ring-gray-300 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" type="button" @click="handleViewAll">清空搜尋條件</button>
+            <button @click="createFormToggle = true" class="flex select-none items-center gap-3 rounded-lg bg-primary-600 py-2 px-4 text-center text-xs font-bold text-white shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-primary-600/20 focus:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" type="button">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" stroke-width="2" class="w-4 h-4">
                 <path d="M6.25 6.375a4.125 4.125 0 118.25 0 4.125 4.125 0 01-8.25 0zM3.25 19.125a7.125 7.125 0 0114.25 0v.003l-.001.119a.75.75 0 01-.363.63 13.067 13.067 0 01-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 01-.364-.63l-.001-.122zM19.75 7.5a.75.75 0 00-1.5 0v2.25H16a.75.75 0 000 1.5h2.25v2.25a.75.75 0 001.5 0v-2.25H22a.75.75 0 000-1.5h-2.25V7.5z"></path>
               </svg>
@@ -121,15 +189,17 @@ export default {
           <div class="w-full md:w-max">
             <nav>
               <ul role="tablist" class="relative flex flex-row p-1 min-w-[300px] rounded-lg bg-primary-100 bg-opacity-60">
-                <li role="tab" class="relative flex items-center justify-center w-full h-full px-2 py-1 text-center cursor-pointer select-none text-primary-900">
-                  <div class="z-10">&nbsp;&nbsp;All&nbsp;&nbsp;</div>
-                  <div class="absolute inset-0 h-full bg-white rounded-md shadow z-5"></div>
+                <li role="tab" class="relative flex items-center justify-center w-full h-full px-2 py-1 text-center cursor-pointer select-none text-primary-900" @click="this.filterType = 'all'">
+                  <div class="z-10">All</div>
+                  <div v-if="filterType == 'all'" class="absolute inset-0 h-full bg-white rounded-md shadow z-5"></div>
                 </li>
-                <li role="tab" class="relative flex items-center justify-center w-full h-full px-2 py-1 text-center cursor-pointer select-none text-primary-900">
-                  <div class="z-10">&nbsp;&nbsp;住院&nbsp;&nbsp;</div>
+                <li role="tab" class="relative flex items-center justify-center w-full h-full px-2 py-1 text-center cursor-pointer select-none text-primary-900" @click="this.filterType = 'dog'">
+                  <div class="z-10"><i class="fa-solid fa-dog"></i></div>
+                  <div v-if="filterType == 'dog'" class="absolute inset-0 h-full bg-white rounded-md shadow z-5"></div>
                 </li>
-                <li role="tab" class="relative flex items-center justify-center w-full h-full px-2 py-1 text-center cursor-pointer select-none text-primary-900">
-                  <div class="z-10">&nbsp;&nbsp;出院&nbsp;&nbsp;</div>
+                <li role="tab" class="relative flex items-center justify-center w-full h-full px-2 py-1 text-center cursor-pointer select-none text-primary-900" @click="this.filterType = 'cat'">
+                  <div class="z-10"><i class="fa-solid fa-cat"></i></div>
+                  <div v-if="filterType == 'cat'" class="absolute inset-0 h-full bg-white rounded-md shadow z-5"></div>
                 </li>
               </ul>
             </nav>
@@ -205,13 +275,13 @@ export default {
             </tr>
           </thead>
           <tbody>
-            <tr class="hover:bg-primary-100" v-for="animal in animalList" :key="animal._id" @click="this.$router.push(`/animal/${animal._id}`)">
+            <tr class="hover:bg-primary-100" v-for="animal in showData" :key="animal._id" @click="this.$router.push(`/animal/${animal._id}`)">
               <td class="p-4 border-b border-primary-50">
                 <div class="flex items-center gap-3">
                   <img src="/image/1.jpg" alt="John Michael" class="relative inline-block h-9 w-9 !rounded-full object-cover object-center" />
                   <div class="flex flex-col">
-                    <p class="block font-sans text-sm antialiased font-bold leading-normal text-primary-900">{{ animal.name }}</p>
-                    <p class="block font-sans text-sm antialiased font-normal leading-normal text-primary-900 opacity-70">{{ animal.type == 'dog' ? '狗狗' : '貓貓' }} {{ convertBirthdayToAge(animal.birthday).years }}歲 {{ convertBirthdayToAge(animal.birthday).months > 0 ? convertBirthdayToAge(animal.birthday).months + '個月' : '' }}</p>
+                    <p class="block font-sans text-sm antialiased font-bold leading-normal text-primary-900"><i :class="animal.gender === 'male' ? 'text-primary-600 fa-solid fa-mars' : 'text-pink-600 fa-solid fa-venus'"></i> {{ animal.name }}</p>
+                    <p class="block font-sans text-sm antialiased font-normal leading-normal text-primary-900 opacity-70"><i :class="['fa-solid', animalIcon(animal.type)]"> </i> {{ convertBirthdayToAge(animal.birthday).years }}歲 {{ convertBirthdayToAge(animal.birthday).months > 0 ? convertBirthdayToAge(animal.birthday).months + '個月' : '' }}</p>
                   </div>
                 </div>
               </td>
@@ -240,8 +310,8 @@ export default {
               <td class="p-4 border-b border-primary-50">
                 <p class="block font-sans text-sm antialiased font-normal leading-normal text-primary-900">{{ new Date(animal.admissionDate).toISOString().slice(0, 10) }}</p>
               </td>
-              <td class="p-4 border-b border-primary-50">
-                <button class="relative h-10 max-h-[40px] w-10 max-w-[40px] select-none rounded-lg text-center align-middle font-sans text-xs font-medium uppercase text-gray-900 transition-all hover:bg-gray-900/10 active:bg-gray-900/20 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" type="button">
+              <td class="text-center border-b border-primary-50" @click="editToggle($event, animal._id)">
+                <button class="relative h-12 max-h-[60px] w-12 max-w-[60px] select-none rounded-lg text-center align-middle font-sans text-xs font-medium uppercase text-gray-900 transition-all hover:bg-gray-900/10 active:bg-gray-900/20 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" type="button">
                   <span class="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
                     <i class="text-xl text-primary-900 fa-solid fa-pen-to-square"></i>
                   </span>
@@ -259,7 +329,7 @@ export default {
         </div>
       </div>
     </div>
-    <div v-if="iscreateFormOpen" class="fixed inset-0 z-10 flex items-center justify-center bg-black bg-opacity-70">
+    <div v-if="createFormToggle" class="fixed inset-0 z-10 flex items-center justify-center bg-black bg-opacity-70">
       <div class="bg-primary-50 p-4 lg:p-8 rounded-xl text-center w-[100%] max-w-2xl">
         <h2 class="mb-4 text-xl font-semibold text-primary-900">新增動物資料</h2>
         <div class="mb-6">
@@ -351,8 +421,105 @@ export default {
           </div>
         </div>
         <div class="flex justify-between">
-          <button class="w-1/3 px-6 py-2 text-gray-700 transition-all bg-gray-300 rounded-lg shadow-md hover:bg-gray-400" @click="iscreateFormOpen = false">取消</button>
+          <button class="w-1/3 px-6 py-2 text-gray-700 transition-all bg-gray-300 rounded-lg shadow-md hover:bg-gray-400" @click="createFormToggle = false">取消</button>
           <button class="w-1/3 px-6 py-2 text-white transition-all rounded-lg shadow-md bg-primary-500 hover:bg-primary-400" @click="creatNewAnimal">確定</button>
+        </div>
+      </div>
+    </div>
+    <div v-if="editFormToggle" class="fixed inset-0 z-10 flex items-center justify-center bg-black bg-opacity-70">
+      <div class="bg-primary-50 p-4 lg:p-8 rounded-xl text-center w-[100%] max-w-2xl">
+        <h2 class="mb-4 text-xl font-semibold text-primary-900">修改動物資料</h2>
+        <div class="mb-6">
+          <div class="space-y-4">
+            <div class="flex items-center">
+              <label for="name" class="w-1/3 text-left text-primary-700">姓名</label>
+              <input id="name" type="text" placeholder="姓名" class="w-full p-2 border rounded-lg shadow-sm border-primary-200 focus:outline-none focus:ring-2 focus:ring-primary-300" v-model="editForm.name" autocomplete="off" />
+            </div>
+            <div class="flex items-center">
+              <label for="gender" class="w-1/3 text-left text-primary-700">性別</label>
+              <select id="gender" class="block w-full p-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-primary-300" v-model="editForm.gender">
+                <option value="male">男生</option>
+                <option value="female">女生</option>
+              </select>
+            </div>
+            <div class="flex items-center">
+              <label for="weight" class="w-1/3 text-left text-primary-700">體重 (kg)</label>
+              <input id="weight" type="number" placeholder="體重 (kg)" class="w-full p-2 border rounded-lg shadow-sm border-primary-200 focus:outline-none focus:ring-2 focus:ring-primary-300" v-model="editForm.weight" />
+            </div>
+            <div class="flex items-center">
+              <label for="birthday" class="w-1/3 text-left text-primary-700">生日</label>
+              <input id="birthday" ref="birthday" type="date" class="block w-full p-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-primary-300" @focus="$refs.birthday.showPicker()" v-model="editForm.birthday" />
+            </div>
+            <div class="flex items-center">
+              <label class="w-1/3 text-left text-primary-700">是否結紮</label>
+              <div class="flex justify-around w-full">
+                <select id="neutered" class="block w-full p-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-primary-300" v-model="editForm.sterilized">
+                  <option value="true">已結紮</option>
+                  <option value="false">未結紮</option>
+                </select>
+              </div>
+            </div>
+            <div class="flex items-center">
+              <label for="breed" class="w-1/3 text-left text-primary-700">品種</label>
+              <input id="breed" type="text" placeholder="品種" class="block w-full p-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-primary-300" v-model="editForm.breed" autocomplete="off" />
+            </div>
+
+            <div class="flex items-center">
+              <label class="w-1/3 text-left text-primary-700">血型</label>
+              <div class="flex justify-between w-full">
+                <label class="rounded-lg">
+                  <input type="radio" name="editBloodType" value="A" class="hidden peer" v-model="editForm.bloodType" />
+                  <span class="px-2 py-2 rounded-lg shadow-md cursor-pointer bg-primary-100 lg:px-4 text-primary-800 peer-checked:bg-primary-500 peer-checked:text-white"><i class="fa-solid fa-a"></i> 型</span>
+                </label>
+                <label class="rounded-l">
+                  <input type="radio" name="editBloodType" value="B" class="hidden peer" v-model="editForm.bloodType" />
+                  <span class="px-2 py-2 transition-all rounded-lg shadow-md cursor-pointer lg:px-4 text-primary-800 bg-primary-100 peer-checked:bg-primary-500 peer-checked:text-white"><i class="fa-solid fa-b"></i> 型</span>
+                </label>
+                <label class="rounded-l">
+                  <input type="radio" name="editBloodType" value="AB" class="hidden peer" v-model="editForm.bloodType" />
+                  <span class="px-2 py-2 transition-all rounded-lg shadow-md cursor-pointer lg:px-4 text-primary-800 bg-primary-100 peer-checked:bg-primary-500 peer-checked:text-white"><i class="fa-solid fa-a"></i><i class="fa-solid fa-b"></i> 型</span>
+                </label>
+                <label class="rounded-l">
+                  <input type="radio" name="editBloodType" value="O" class="hidden peer" v-model="editForm.bloodType" />
+                  <span class="px-2 py-2 transition-all rounded-lg shadow-md cursor-pointer lg:px-4 text-primary-800 bg-primary-100 peer-checked:bg-primary-500 peer-checked:text-white"><i class="fa-solid fa-o"></i> 型</span>
+                </label>
+              </div>
+            </div>
+
+            <div class="flex items-center">
+              <label for="species" class="w-1/3 text-left text-primary-700">種類</label>
+              <div class="flex items-center justify-between w-full gap-2">
+                <label class="min-w-[70px] h-full rounded-lg">
+                  <input type="radio" name="editType" value="cat" class="hidden peer" v-model="editForm.type" />
+                  <span class="px-2 py-2 rounded-lg shadow-md cursor-pointer bg-primary-100 text-primary-800 peer-checked:bg-primary-500 peer-checked:text-white"><i class="fa-solid fa-cat"></i> 貓貓 </span>
+                </label>
+                <label class="min-w-[70px] rounded-l">
+                  <input type="radio" name="editType" value="dog" class="hidden peer" v-model="editForm.type" />
+                  <span class="px-2 py-2 transition-all rounded-lg shadow-md cursor-pointer lg:px-2 text-primary-800 bg-primary-100 peer-checked:bg-primary-500 peer-checked:text-white"><i class="fa-solid fa-dog"></i> 狗狗 </span>
+                </label>
+                <label class="min-w-[48px] rounded-l">
+                  <input type="radio" name="editType" value="other" class="hidden peer" v-model="editForm.type" />
+                  <span class="px-2 py-2 transition-all rounded-lg shadow-md cursor-pointer lg:px-2 text-primary-800 bg-primary-100 peer-checked:bg-primary-500 peer-checked:text-white">其他 </span>
+                </label>
+                <!-- <label class="rounded-l">
+                  <input type="text" placeholder="其他" class="w-full p-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-primary-300" />
+                </label> -->
+              </div>
+            </div>
+
+            <div class="flex items-center">
+              <label for="insulinBrand" class="w-1/3 text-left text-primary-700">胰島素品牌</label>
+              <input id="insulinBrand" type="text" placeholder="胰島素品牌" class="block w-full p-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-primary-300" v-model="editForm.insulinBrand" autocomplete="off" />
+            </div>
+            <div class="flex items-center">
+              <label for="admissionDate" class="w-1/3 text-left text-primary-700">入院日期</label>
+              <input ref="admissionDate" type="date" class="block w-full p-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-primary-300" @focus="$refs.admissionDate.showPicker()" v-model="editForm.admissionDate" />
+            </div>
+          </div>
+        </div>
+        <div class="flex justify-between">
+          <button class="w-1/3 px-6 py-2 text-gray-700 transition-all bg-gray-300 rounded-lg shadow-md hover:bg-gray-400" @click="editFormToggle = false">取消</button>
+          <button class="w-1/3 px-6 py-2 text-white transition-all rounded-lg shadow-md bg-primary-500 hover:bg-primary-400" @click="editAnimal">確定</button>
         </div>
       </div>
     </div>
