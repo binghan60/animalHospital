7bc31a373a051f8fa15e37e5049e1fb46a52eedf
binghan <binghan60@gmail.com>
2024-12-22 23:54:42 +0800

    client:navbar調整排版

--- client/src/components/NavbarComponent.vue
+++ client/src/components/NavbarComponent.vue
@@ -1,7 +1,11 @@
 <script>
+import { RouterLink } from 'vue-router'
 import { mapActions, mapState } from 'pinia'
 import authStore from '@/stores/auth.js'
 export default {
+  components: {
+    RouterLink,
+  },
   data() {
     return {
       isMenuOpen: false,
@@ -32,12 +36,14 @@ export default {
 <template>
   <nav class="mb-0 lg:mb-8 bg-primary-400">
     <div class="flex items-center justify-between p-2 mx-auto text-lg text-white max-w-7xl">
-      <a @click="chagnePage('/animallist')">
+      <RouterLink to="/animallist" @click="isMenuOpen = false">
         <div class="flex items-center text-xl font-bold cursor-pointer">
-          <img class="w-full h-full lg:max-w-[50px] max-w-[35px]" src="/image/logo.svg" alt="Logo" />
-          {{ user.name }}
+          <img class="w-full h-full lg:max-w-[50px] max-w-[40px] pr-2" src="/image/logo.svg" alt="Logo" />
+          <div class="hidden lg:block">
+            {{ user.name }}
+          </div>
         </div>
-      </a>
+      </RouterLink>
       <div class="cursor-pointer lg:hidden" @click="toggleMenu"><span class="text-2xl">☰</span></div>
       <ul :class="['lg:flex lg:space-x-6 lg:space-y-0 absolute lg:static top-12 right-0 w-full text-center lg:w-auto transition-all duration-500 ease-out lg:max-h-full max-h-0 overflow-hidden', { 'max-h-72': isMenuOpen }]">
         <li class="relative z-10 flex items-center justify-center h-10 cursor-pointer bg-primary-400" @click="chagnePage('/animallist')">動物列表</li>
