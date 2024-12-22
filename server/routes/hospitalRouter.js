34e4c39f9e5ac7a1b6fcda693cb486757b566caf
binghan <binghan60@gmail.com>
2024-12-22 22:09:34 +0800

    server:醫院API

--- server/routes/hospitalRouter.js
+++ server/routes/hospitalRouter.js
@@ -32,46 +32,50 @@ router.post('/register', async (req, res) => {
 router.post('/login', async (req, res) => {
     try {
         const { account, password } = req.body;
-        if (account && password) {
-            const hospital = await Hospital.findOne({ account }).lean();
-            if (!hospital) {
-                res.status(400).send({ message: '帳號或密碼錯誤' });
-            }
-            const isMatch = await bcrypt.compare(password, hospital.password);
-            if (!isMatch) {
-                return res.status(400).json({ message: '帳號或密碼錯誤' });
-            }
-            if (!hospital.isActive) {
-                return res.status(400).json({ message: '帳號已停用' });
-            }
-            const payload = {
-                _id: hospital._id,
-            };
-            const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '7d' });
-            const expired = Date.now() + 7 * 24 * 60 * 60 * 1000;
-            return res.status(200).json({ ...hospital, message: '登入成功', token, expired });
+        if (!account || !password) {
+            return res.status(400).send({ message: '帳號或密碼錯誤' });
         }
-        const authorization = req.headers.authorization;
-        const token = authorization?.slice(7);
-        if (token) {
-            jwt.verify(authorization, process.env.JWT_SECRET, async (err, decoded) => {
-                if (err) {
-                    return res.status(400).json({ message: '登入失敗' });
-                }
-                const hospital = await Hospital.findById(decoded._id).lean();
-                delete hospital.password;
-                if (!hospital) {
-                    return res.status(400).json({ message: '登入失敗' });
-                }
-                if (!hospital.isActive) {
-                    return res.status(400).json({ message: '登入失敗' });
-                }
-                return res.status(200).json({ ...hospital, message: '登入成功', token });
-            });
+        const hospital = await Hospital.findOne({ account }).lean();
+        if (!hospital) {
+            return res.status(400).send({ message: '帳號或密碼錯誤' });
         }
+        const isMatch = await bcrypt.compare(password, hospital.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: '帳號或密碼錯誤' });
+        }
+        if (!hospital.isActive) {
+            return res.status(400).json({ message: '帳號已停用' });
+        }
+        const payload = {
+            _id: hospital._id,
+        };
+        delete hospital.password;
+        const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '7d' });
+        const expiresAt = Date.now() + 7 * 24 * 60 * 60 * 1000;
+        return res.status(200).json({ ...hospital, message: '登入成功', token, expiresAt, role: 'hospital' });
     } catch (error) {
         console.error(error);
         res.status(500).send({ message: '伺服器錯誤' });
+    }
+});
+router.post('/tokenLogin', async (req, res) => {
+    const authorization = req.headers.authorization;
+    const token = authorization?.slice(7);
+    if (token) {
+        jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
+            if (err) {
+                return res.status(400).json({ message: '登入失敗' });
+            }
+            const hospital = await Hospital.findById(decoded._id).lean();
+            delete hospital.password;
+            if (!hospital) {
+                return res.status(400).json({ message: '登入失敗' });
+            }
+            if (!hospital.isActive) {
+                return res.status(400).json({ message: '登入失敗' });
+            }
+            return res.status(200).json({ ...hospital, message: '登入成功', token });
+        });
     }
 });
 export default router;
