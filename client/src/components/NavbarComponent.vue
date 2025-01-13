743b139363af2ee1ff25e8eadda02f676829c168
binghan <binghan60@gmail.com>
2025-01-13 20:50:08 +0800

    client:新增管理面板

--- client/src/components/NavbarComponent.vue
+++ client/src/components/NavbarComponent.vue
@@ -32,11 +32,10 @@ export default {
   mounted() {},
 }
 </script>
-
 <template>
   <nav class="mb-0 lg:mb-8 bg-primary-400">
     <div class="flex items-center justify-between p-2 mx-auto text-lg text-white max-w-7xl">
-      <RouterLink to="/animallist" @click="isMenuOpen = false">
+      <RouterLink to="/dashboard" @click="isMenuOpen = false">
         <div class="flex items-center text-xl font-bold cursor-pointer">
           <img class="w-full h-full lg:max-w-[50px] max-w-[40px] pr-2" src="/logo.svg" alt="Logo" />
           <div class="hidden lg:block">
@@ -47,7 +46,6 @@ export default {
       <div class="cursor-pointer lg:hidden" @click="toggleMenu"><span class="text-2xl">☰</span></div>
       <ul :class="['lg:flex lg:space-x-6 lg:space-y-0 absolute lg:static top-12 right-0 w-full text-center lg:w-auto transition-all duration-500 ease-out lg:max-h-full max-h-0 overflow-hidden', { 'max-h-72': isMenuOpen }]">
         <li class="relative z-10 flex items-center justify-center h-10 cursor-pointer bg-primary-400" @click="chagnePage('/animallist')">動物列表</li>
-        <!-- <li class="relative z-10 items-center justify-center hidden h-10 bg-primary-400 lg:flex">{{ user.name }}</li> -->
         <li class="relative z-10 flex items-center justify-center h-10 cursor-pointer bg-primary-400" @click="logOut">登出</li>
       </ul>
     </div>
