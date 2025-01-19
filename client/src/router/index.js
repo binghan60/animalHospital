3c31b3425b6429dfec03c17aa9a5672ba55412e2
binghan <binghan60@gmail.com>
2025-01-19 21:36:39 +0800

    client:路由守衛更新role

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -4,25 +4,47 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: () => import('../views/HomeView.vue'),
+      path: '/hospital',
+      name: 'hospital',
       children: [
-        { path: 'animallist', component: () => import('@/views/AnimalListView.vue') },
+        { path: 'animallist', component: () => import('@/views/hospital/AnimalListView.vue') },
         {
           path: 'animal/:id',
           name: 'animal-detail',
-          component: () => import('@/views/AnimalDetailView.vue'),
+          component: () => import('@/views/hospital/AnimalDetailView.vue'),
           props: true,
         },
         {
           path: 'dashboard',
           name: 'dashboard',
-          component: () => import('@/views/DashboardView.vue'),
+          component: () => import('@/views/hospital/DashboardView.vue'),
           props: true,
         },
       ],
-      meta: { requiresAuth: true },
+      meta: { requiresAuth: true, roles: ['hospital'] },
+    },
+    {
+      path: '/user',
+      name: 'user',
+      meta: { requiresAuth: true, roles: ['user'] },
+      children: [
+        {
+          path: 'profile',
+          name: 'user-profile',
+          component: () => import('@/views/user/UserProfile.vue'),
+        },
+        {
+          path: 'animallist',
+          name: 'user-animal-list',
+          component: () => import('@/views/user/AnimalListView.vue'),
+        },
+        {
+          path: 'animal/:id',
+          name: 'user-animal-detail',
+          component: () => import('@/views/user/AnimalDetailView.vue'),
+          props: true, //將ID透過props傳入，而不是this.$route.params
+        },
+      ],
     },
     {
       path: '/login',
@@ -34,22 +56,20 @@ const router = createRouter({
       name: 'register',
       component: () => import('@/views/RegisterView.vue'),
     },
-    // {
-    // path: '/about',
-    // name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (About.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    //   component: () => import('../views/AboutView.vue'),
-    // },
   ],
 })
 // 進每個路由都會經過 路由守衛
 router.beforeEach((to, from, next) => {
   const store = authStore()
-  if (to.meta.requiresAuth && !store.user.isLogin) {
+  const user = store.user
+  // 檢查是否需要登入
+  if (to.meta.requiresAuth && !user.isLogin) {
     store.setRedirectPath(to.fullPath)
-    next('/login')
+    next('/login') // 沒登入轉到LOGIN
+  } else if (to.meta.roles && !to.meta.roles.includes(user.role)) {
+    // 檢查角色權限
+    console.warn(`權限不足，${user.role} 無法進入 ${to.name}`)
+    next('/login') // 跳轉到LOGIN
   } else {
     next()
   }
