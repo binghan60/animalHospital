46cf4045acac66c1b1c7c2a58b047cc5b225796d
binghan <binghan60@gmail.com>
2025-01-29 01:55:51 +0800

    client:統一name欄位名稱

--- client/src/views/RegisterView.vue
+++ client/src/views/RegisterView.vue
@@ -10,9 +10,9 @@ export default {
   data() {
     return {
       registerForm: {
-        role: '',
+        role: 'hospital',
         account: '',
-        nickname: '',
+        name: '',
         password: '',
         confirmPassword: '',
         showPassword: false,
@@ -23,11 +23,11 @@ export default {
   methods: {
     async register() {
       try {
-        const { account, nickname, password, role } = this.registerForm
+        const { account, name, password, role } = this.registerForm
         const payload = {
           account,
           password,
-          nickname,
+          name,
         }
         const { data } = await axios.post(`${import.meta.env.VITE_API_PATH}/${role}/register`, payload, {
           headers: {
@@ -55,15 +55,20 @@ export default {
           <label for="role" class="text-primary-900 dark:text-darkPrimary-50">請選擇註冊身份*</label>
           <VField id="role" v-model="registerForm.role" name="role" as="select" rules="required" class="w-full h-8 pl-3 mt-2 rounded-md shadow-sm dark:text-darkPrimary-50 dark:bg-darkPrimary-500 text-primary-900 outline-1 outline-primary-100 focus:outline-2 focus:outline-primary-400 dark:placeholder-darkPrimary-400 dark:focus:outline-darkPrimary-400 focus:outline-none">
             <option value="" disabled selected>請選擇</option>
-            <option value="user">飼主</option>
             <option value="hospital">醫院</option>
+            <option value="user">飼主</option>
           </VField>
           <ErrorMessage class="mt-1 text-sm text-red-600 dark:text-rose-400" name="role" />
         </div>
-        <div class="mb-4">
+        <div v-show="registerForm.role == 'hospital'" class="mb-4">
           <label for="account" class="text-primary-900 dark:text-darkPrimary-50">帳號*</label>
           <VField id="account" v-model="registerForm.account" type="text" name="account" rules="required|length:4,20" class="w-full h-8 pl-3 mt-2 rounded-md shadow-sm dark:bg-darkPrimary-500 dark:text-darkPrimary-50 text-primary-900 outline-1 outline-primary-100 focus:outline-2 focus:outline-primary-400 dark:placeholder-darkPrimary-400 dark:focus:outline-darkPrimary-400 focus:outline-none" placeholder="請輸入帳號" autocomplete="off" />
           <ErrorMessage class="mt-1 text-sm text-red-600 dark:text-rose-400" name="account" />
+        </div>
+        <div v-show="registerForm.role == 'user'" class="mb-4">
+          <label for="misdn" class="text-primary-900 dark:text-darkPrimary-50">帳號*</label>
+          <VField id="misdn" v-model="registerForm.account" type="tel" name="misdn" rules="required|length:4,10" class="w-full h-8 pl-3 mt-2 rounded-md shadow-sm dark:bg-darkPrimary-500 dark:text-darkPrimary-50 text-primary-900 outline-1 outline-primary-100 focus:outline-2 focus:outline-primary-400 dark:placeholder-darkPrimary-400 dark:focus:outline-darkPrimary-400 focus:outline-none" placeholder="請輸入飼主手機號碼" autocomplete="off" />
+          <ErrorMessage class="mt-1 text-sm text-red-600 dark:text-rose-400" name="misdn" />
         </div>
         <div class="mb-4">
           <label for="password" class="text-primary-900 dark:text-darkPrimary-50">密碼*</label>
@@ -87,16 +92,15 @@ export default {
           </div>
           <ErrorMessage class="mt-1 text-sm text-red-600 dark:text-rose-400" name="confirmPassword" />
         </div>
-
-        <div v-show="registerForm.role === 'user'" class="mb-4">
-          <label for="nickname" class="text-primary-900 dark:text-darkPrimary-50">暱稱*</label>
-          <VField id="nickname" v-model="registerForm.nickname" rules="required|length:1,20" type="text" name="nickname" class="w-full h-8 pl-3 mt-2 rounded-md shadow-sm dark:bg-darkPrimary-500 dark:text-darkPrimary-50 text-primary-900 outline-1 outline-primary-100 focus:outline-2 focus:outline-primary-400 dark:placeholder-darkPrimary-400 dark:focus:outline-darkPrimary-400 focus:outline-none" placeholder="請輸入暱稱" autocomplete="off" />
-          <ErrorMessage class="mt-1 text-sm text-red-600 dark:text-rose-400" name="nickname" />
-        </div>
         <div v-show="registerForm.role === 'hospital'" class="mb-4">
           <label for="hospitalName" class="text-primary-900 dark:text-darkPrimary-50">醫院名稱</label>
-          <VField id="hospitalName" v-model="registerForm.hospitalName" type="text" name="hospitalName" class="w-full h-8 pl-3 mt-2 rounded-md shadow-sm dark:bg-darkPrimary-500 dark:text-darkPrimary-50 text-primary-900 outline-1 outline-primary-100 focus:outline-2 focus:outline-primary-400 dark:placeholder-darkPrimary-400 dark:focus:outline-darkPrimary-400 focus:outline-none" placeholder="請輸入醫院名稱" autocomplete="off" />
+          <VField id="hospitalName" v-model="registerForm.name" type="text" name="hospitalName" class="w-full h-8 pl-3 mt-2 rounded-md shadow-sm dark:bg-darkPrimary-500 dark:text-darkPrimary-50 text-primary-900 outline-1 outline-primary-100 focus:outline-2 focus:outline-primary-400 dark:placeholder-darkPrimary-400 dark:focus:outline-darkPrimary-400 focus:outline-none" placeholder="請輸入醫院名稱" autocomplete="off" />
           <ErrorMessage class="mt-1 text-sm text-red-600 dark:text-rose-400" name="hospitalName" />
+        </div>
+        <div v-show="registerForm.role === 'user'" class="mb-4">
+          <label for="name" class="text-primary-900 dark:text-darkPrimary-50">暱稱</label>
+          <VField id="name" v-model="registerForm.name" rules="length:1,20" type="text" name="name" class="w-full h-8 pl-3 mt-2 rounded-md shadow-sm dark:bg-darkPrimary-500 dark:text-darkPrimary-50 text-primary-900 outline-1 outline-primary-100 focus:outline-2 focus:outline-primary-400 dark:placeholder-darkPrimary-400 dark:focus:outline-darkPrimary-400 focus:outline-none" placeholder="請輸入暱稱" autocomplete="off" />
+          <ErrorMessage class="mt-1 text-sm text-red-600 dark:text-rose-400" name="name" />
         </div>
 
         <button type="submit" class="w-full px-4 py-2 mt-4 text-white rounded-md bg-primary-600 dark:bg-indigo-600 hover:dark:bg-indigo-700 hover:bg-primary-700 outline-1 focus:outline-2 focus:outline-primary-500 focus:outline-offset-2 focus:outline-none">註冊</button>
