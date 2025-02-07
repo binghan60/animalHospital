17a317d2e1f5c85f6bce677fc914a78ac25cdf28
binghan <binghan60@gmail.com>
2025-02-08 02:47:04 +0800

    client:新增按鈕Loading動畫

--- client/src/views/LoginView.vue
+++ client/src/views/LoginView.vue
@@ -69,8 +69,8 @@ export default {
 }
 </script>
 <template>
-  <div class="flex items-center justify-center min-h-screen">
-    <div class="w-full h-full max-w-sm min-w-[350px] p-6 lg:bg-white bg-primary-50 dark:lg:bg-darkPrimary-700 rounded-lg dark:bg-darkPrimary-800 lg:shadow-lg dark:shadow-none lg:p-8">
+  <div class="flex items-center justify-center lg:pb-0 pb-[10px]" style="height: calc(100vh - 58px - 4rem)">
+    <div class="w-full max-w-sm min-w-[350px] max-h-[800px] p-6 lg:bg-white bg-primary-50 dark:lg:bg-darkPrimary-700 rounded-lg dark:bg-darkPrimary-800 lg:shadow-lg dark:shadow-none lg:p-8">
       <h2 class="mb-6 text-2xl font-bold text-center text-primary-900 dark:text-darkPrimary-50">動物健康管理系統</h2>
       <VForm @submit="login">
         <div class="mb-4">
@@ -97,7 +97,11 @@ export default {
           </div>
           <ErrorMessage class="mt-1 text-sm text-red-600 dark:text-rose-400" name="password" />
         </div>
-        <button type="submit" class="w-full px-4 py-2 mt-4 text-white rounded-md bg-primary-600 dark:bg-indigo-600 hover:dark:bg-indigo-700 hover:bg-primary-700 outline-1 focus:outline-2 focus:outline-primary-500 focus:outline-offset-2 focus:outline-none">登入</button>
+        <button v-if="isLoading" class="inline-flex items-center justify-center w-full px-4 py-2 mt-4 rounded-md bg-primary-600 dark:bg-indigo-600 text-darkPrimary-50 disabled:opacity-50 disabled:cursor-not-allowed disabled:pointer-events-auto" disabled="">
+          <div class="w-4 h-4 border-2 border-white border-solid rounded-full animate-spin border-t-transparent"></div>
+          <span class="ml-2">登入中... </span>
+        </button>
+        <button v-else type="submit" class="w-full px-4 py-2 mt-4 text-white rounded-md bg-primary-600 dark:bg-indigo-600 hover:dark:bg-indigo-700 hover:bg-primary-700 outline-1 focus:outline-2 focus:outline-primary-500 focus:outline-offset-2 focus:outline-none">登入</button>
         <div class="flex justify-between">
           <p class="mt-4 text-sm text-center text-primary-900 dark:text-darkPrimary-400">沒有帳號？<RouterLink to="/register" class="text-primary-600 hover:underline dark:text-darkPrimary-50">註冊</RouterLink></p>
           <p class="mt-4 text-sm text-center text-primary-900 dark:text-darkPrimary-400"><RouterLink to="/forget-password" class="text-primary-600 hover:underline dark:text-darkPrimary-50">忘記密碼</RouterLink></p>
@@ -106,6 +110,6 @@ export default {
       <button type="submit" class="w-full px-4 py-2 my-2 text-white bg-orange-500 rounded-md dark:bg-amber-600 hover:dark:bg-amber-700 hover:bg-orange-600 outline-1 focus:outline-2 focus:outline-primary-500 focus:outline-offset-2 focus:outline-none" @click="quicklogin">醫院DEMO帳號</button>
       <button type="submit" class="w-full px-4 py-2 my-2 text-white bg-green-600 rounded-md dark:bg-lime-600 hover:dark:bg-lime-700 hover:bg-green-700 outline-1 focus:outline-2 focus:outline-primary-500 focus:outline-offset-2 focus:outline-none" @click="userlogin">飼主DEMO帳號</button>
     </div>
-    <VueLoading :active="isLoading" :height="loadingConfig.height" :width="loadingConfig.width" :loader="loadingConfig.loader" :color="loadingConfig.getColor()" />
+    <!-- <VueLoading :active="isLoading" :height="loadingConfig.height" :width="loadingConfig.width" :loader="loadingConfig.loader" :color="loadingConfig.getColor()" :backgroundColor="loadingConfig.backgroundColor()" /> -->
   </div>
 </template>
