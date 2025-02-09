020e66afd7d91f3046a04bed65689a6b38ccff6f
binghan <binghan60@gmail.com>
2025-02-09 20:28:45 +0800

    client:調整外框顏色

--- client/src/views/LoginView.vue
+++ client/src/views/LoginView.vue
@@ -108,8 +108,8 @@ export default {
           <p class="mt-4 text-sm text-center text-primary-900 dark:text-darkPrimary-400"><RouterLink to="/forget-password" class="text-primary-600 hover:underline dark:text-darkPrimary-50">忘記密碼</RouterLink></p>
         </div>
       </VForm>
-      <button type="submit" class="w-full px-4 py-2 my-2 text-white bg-orange-500 rounded-md dark:bg-amber-600 hover:dark:bg-amber-700 hover:bg-orange-600 outline-1 focus:outline-2 focus:outline-primary-500 focus:outline-offset-2 focus:outline-none" @click="quicklogin">醫院DEMO帳號</button>
-      <button type="submit" class="w-full px-4 py-2 my-2 text-white bg-green-600 rounded-md dark:bg-lime-600 hover:dark:bg-lime-700 hover:bg-green-700 outline-1 focus:outline-2 focus:outline-primary-500 focus:outline-offset-2 focus:outline-none" @click="userlogin">飼主DEMO帳號</button>
+      <button type="submit" class="w-full px-4 py-2 my-2 text-white bg-orange-500 rounded-md dark:bg-amber-600 hover:dark:bg-amber-700 hover:bg-orange-600 outline-1 focus:outline-2 focus:outline-amber-500 focus:outline-offset-2 focus:outline-none" @click="quicklogin">醫院DEMO帳號</button>
+      <button type="submit" class="w-full px-4 py-2 my-2 text-white bg-green-600 rounded-md dark:bg-lime-600 hover:dark:bg-lime-700 hover:bg-green-700 outline-1 focus:outline-2 focus:outline-green-500 focus:outline-offset-2 focus:outline-none" @click="userlogin">飼主DEMO帳號</button>
     </div>
     <!-- <VueLoading :active="isLoading" :height="loadingConfig.height" :width="loadingConfig.width" :loader="loadingConfig.loader" :color="loadingConfig.getColor()" :backgroundColor="loadingConfig.backgroundColor()" /> -->
   </div>
