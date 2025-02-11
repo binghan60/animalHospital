62786921a65480604b72b249ed984f43752f719a
binghan <binghan60@gmail.com>
2025-02-12 02:49:17 +0800

    server:修復生日計算方式

--- server/routes/animalRouter.js
+++ server/routes/animalRouter.js
@@ -42,8 +42,10 @@ router.post('/create', upload.single('avatar'), async (req, res) => {
             res.status(400).send({ message: '缺少參數：name' });
             return;
         }
+        console.log(birthday);
+        console.log(parseInt(birthday));
 
-        if (typeof birthday === 'number') {
+        if (parseInt(birthday) < 1000) {
             const currentDate = new Date();
             const currentYear = currentDate.getFullYear();
             const currentMonth = currentDate.getMonth();
