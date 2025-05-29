96e6a6986ba1303f3ba7b4141a44f33bc8bb4d30
binghan60 <binghan60@gmail.com>
2025-05-29 11:07:17 +0800

    重構 ChartComponent 和 NavbarComponent，將其轉換為 <script setup> 語法，簡化組件結構

--- client/src/components/ChartComponent.vue
+++ client/src/components/ChartComponent.vue
@@ -1,43 +1,38 @@
 <template>
   <component :is="chartComponent" :key="chartData.labels.length" :data="chartData" :options="chartOptions" />
 </template>
-<script>
+
+<script setup>
+import { computed } from 'vue'
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, LineElement, PointElement, ArcElement, Title, Tooltip, Legend, Filler } from 'chart.js'
-import { Bar, Line, Pie, Doughnut } from 'vue-chartjs'
 import ChartDataLabels from 'chartjs-plugin-datalabels'
+import { Bar, Line, Pie, Doughnut } from 'vue-chartjs'
+
 ChartJS.register(CategoryScale, LinearScale, BarElement, LineElement, PointElement, ArcElement, Title, Tooltip, Legend, ChartDataLabels, Filler)
-export default {
-  name: 'ChartComponent',
-  components: {
-    BarChart: Bar,
-    LineChart: Line,
-    PieChart: Pie,
-    DoughnutChart: Doughnut,
+
+// props可以直接在templete使用，在script使用要自己接住const { type } = definePrope({...
+const props = defineProps({
+  type: {
+    type: String,
+    required: true,
+    validator: value => ['bar', 'line', 'pie', 'doughnut'].includes(value),
   },
-  props: {
-    type: {
-      type: String,
-      required: true,
-      validator: value => ['bar', 'line', 'pie', 'doughnut'].includes(value), // return true
-    },
-    chartData: {
-      type: Object,
-      required: true,
-    },
-    chartOptions: {
-      type: Object,
-      default: () => ({}),
-    },
+  chartData: {
+    type: Object,
+    required: true,
   },
-  computed: {
-    chartComponent() {
-      return {
-        bar: 'BarChart',
-        line: 'LineChart',
-        pie: 'PieChart',
-        doughnut: 'DoughnutChart',
-      }[this.type]
-    },
+  chartOptions: {
+    type: Object,
+    default: () => ({}),
   },
-}
+})
+
+const chartComponent = computed(() => {
+  return {
+    bar: Bar,
+    line: Line,
+    pie: Pie,
+    doughnut: Doughnut,
+  }[props.type]
+})
 </script>
--- client/src/components/NavbarComponent.vue
+++ client/src/components/NavbarComponent.vue
@@ -1,44 +1,37 @@
-<script>
-import { RouterLink } from 'vue-router'
-import { mapActions, mapState } from 'pinia'
-import authStore from '@/stores/auth.js'
-export default {
-  components: {
-    RouterLink,
-  },
-  data() {
-    return {
-      isMenuOpen: false,
-    }
-  },
-  methods: {
-    toggleMenu() {
-      this.isMenuOpen = !this.isMenuOpen
-    },
-    chagnePage(path) {
-      this.$router.push(path)
-      this.toggleMenu()
-    },
-    logOut() {
-      if (this.clearAuth()) {
-        this.$router.push('/login')
-        this.$toast.success('登出成功')
-        this.toggleMenu()
-      }
-    },
-    handleClick() {
-      this.toggleTheme()
-      this.toggleMenu()
-    },
+<script setup>
+import { ref, computed } from 'vue'
+import { useRouter } from 'vue-router'
+import { useAuthStore } from '@/stores/auth.js'
 
-    ...mapActions(authStore, ['clearAuth', 'toggleTheme']),
-  },
-  computed: {
-    ...mapState(authStore, ['user', 'isDark']),
-  },
-  mounted() {},
+const router = useRouter()
+const auth = useAuthStore()
+const user = computed(() => auth.user)
+const isDark = computed(() => auth.isDark)
+
+const isMenuOpen = ref(false)
+
+const toggleMenu = () => {
+  isMenuOpen.value = !isMenuOpen.value
+}
+
+const changePage = path => {
+  router.push(path)
+  toggleMenu()
+}
+
+const logOut = () => {
+  if (auth.clearAuth()) {
+    router.push('/login')
+    window.$toast?.success?.('登出成功')
+    toggleMenu()
+  }
+}
+const handleClick = () => {
+  auth.toggleTheme()
+  toggleMenu()
 }
 </script>
+
 <template>
   <nav class="transition-colors bg-primary-400 dark:bg-darkPrimary-500 h-[58px]">
     <div class="flex items-center justify-between h-full p-2 mx-auto text-lg text-white max-w-7xl">
@@ -46,18 +39,24 @@ export default {
         <div class="flex items-center text-xl font-bold cursor-pointer">
           <img class="w-full h-full lg:max-w-[50px] max-w-[40px] pr-2" src="/logo.svg" alt="Logo" />
           <div class="hidden lg:block">
-            <button role="link" class="relative after:absolute after:bottom-0 after:left-0 after:right-0 after:h-[2px] after:w-full after:origin-bottom after:scale-x-0 after:bg-darkPrimary-50 after:transition-transform after:duration-300 after:ease-[cubic-bezier(0.65_0.05_0.36_1)] hover:after:origin-bottom hover:after:scale-x-100">{{ user.name }}</button>
+            <button role="link" class="relative after:absolute after:bottom-0 after:left-0 after:right-0 after:h-[2px] after:w-full after:origin-bottom after:scale-x-0 after:bg-darkPrimary-50 after:transition-transform after:duration-300 after:ease-[cubic-bezier(0.65_0.05_0.36_1)] hover:after:origin-bottom hover:after:scale-x-100">
+              {{ user.name }}
+            </button>
           </div>
         </div>
       </RouterLink>
+
       <RouterLink v-else-if="user.role === 'user'" to="/user/animallist" @click="isMenuOpen = false">
         <div class="flex items-center text-xl font-bold cursor-pointer">
           <img class="w-full h-full lg:max-w-[50px] max-w-[40px] pr-2" src="/logo.svg" alt="Logo" />
           <div class="hidden lg:block">
-            <button role="link" class="block relative after:absolute after:bottom-0 after:left-0 after:right-0 after:h-[2px] after:w-full after:origin-bottom after:scale-x-0 after:bg-darkPrimary-50 after:transition-transform after:duration-300 after:ease-[cubic-bezier(0.65_0.05_0.36_1)] hover:after:origin-bottom hover:after:scale-x-100">{{ user.name }}</button>
+            <button role="link" class="block relative after:absolute after:bottom-0 after:left-0 after:right-0 after:h-[2px] after:w-full after:origin-bottom after:scale-x-0 after:bg-darkPrimary-50 after:transition-transform after:duration-300 after:ease-[cubic-bezier(0.65_0.05_0.36_1)] hover:after:origin-bottom hover:after:scale-x-100">
+              {{ user.name }}
+            </button>
           </div>
         </div>
       </RouterLink>
+
       <RouterLink v-else to="/" @click="isMenuOpen = false">
         <div class="flex items-center text-xl font-bold cursor-pointer">
           <img class="w-full h-full lg:max-w-[50px] max-w-[40px] pr-2" src="/logo.svg" alt="Logo" />
@@ -66,25 +65,32 @@ export default {
           </div>
         </div>
       </RouterLink>
-      <div class="cursor-pointer lg:hidden" @click="toggleMenu"><span class="text-2xl">☰</span></div>
+
+      <div class="cursor-pointer lg:hidden" @click="toggleMenu">
+        <span class="text-2xl">☰</span>
+      </div>
+
       <ul :class="['lg:flex lg:space-x-6 lg:space-y-0 absolute lg:static top-12 right-0 w-full text-center lg:w-auto transition-all duration-500 ease-out lg:max-h-full max-h-0 overflow-hidden', { 'max-h-72': isMenuOpen }]">
-        <li v-if="user.isLogin" class="relative z-10 flex items-center justify-center h-10 transition-colors cursor-pointer select-none lg:z-0 bg-primary-400 dark:bg-darkPrimary-500" @click="chagnePage('/member')">
-          <button role="link" class="relative after:absolute after:bottom-0 after:left-0 after:right-0 after:h-[2px] after:w-full after:origin-bottom after:scale-x-0 after:bg-darkPrimary-50 after:transition-transform after:duration-300 after:ease-[cubic-bezier(0.65_0.05_0.36_1)] hover:after:origin-bottom hover:after:scale-x-100">會員中心</button>
+        <li v-if="user.isLogin" class="relative z-10 flex items-center justify-center h-10 cursor-pointer select-none bg-primary-400 dark:bg-darkPrimary-500" @click="changePage('/member')">
+          <button role="link" class="relative after:absolute after:bottom-0 after:left-0 after:right-0 after:h-[2px] after:w-full after:scale-x-0 hover:after:scale-x-100 after:bg-darkPrimary-50 after:transition-transform after:duration-300">會員中心</button>
         </li>
-        <li v-if="user.role == 'hospital' && user.isLogin" class="relative z-10 flex items-center justify-center h-10 transition-colors cursor-pointer select-none lg:z-0 bg-primary-400 dark:bg-darkPrimary-500" @click="chagnePage('/hospital/animallist')">
-          <button role="link" class="relative after:absolute after:bottom-0 after:left-0 after:right-0 after:h-[2px] after:w-full after:origin-bottom after:scale-x-0 after:bg-darkPrimary-50 after:transition-transform after:duration-300 after:ease-[cubic-bezier(0.65_0.05_0.36_1)] hover:after:origin-bottom hover:after:scale-x-100">動物列表</button>
+        <li v-if="user.role === 'hospital' && user.isLogin" class="relative z-10 flex items-center justify-center h-10 cursor-pointer select-none bg-primary-400 dark:bg-darkPrimary-500" @click="changePage('/hospital/animallist')">
+          <button role="link" class="relative after:absolute after:bottom-0 after:left-0 after:right-0 after:h-[2px] after:w-full after:scale-x-0 hover:after:scale-x-100 after:bg-darkPrimary-50 after:transition-transform after:duration-300">動物列表</button>
         </li>
-        <li v-if="user.role == 'user' && user.isLogin" class="relative z-10 flex items-center justify-center h-10 transition-colors cursor-pointer select-none lg:z-0 bg-primary-400 dark:bg-darkPrimary-500" @click="chagnePage('/user/animallist')">
-          <button role="link" class="relative after:absolute after:bottom-0 after:left-0 after:right-0 after:h-[2px] after:w-full after:origin-bottom after:scale-x-0 after:bg-darkPrimary-50 after:transition-transform after:duration-300 after:ease-[cubic-bezier(0.65_0.05_0.36_1)] hover:after:origin-bottom hover:after:scale-x-100">動物列表</button>
+        <li v-if="user.role === 'user' && user.isLogin" class="relative z-10 flex items-center justify-center h-10 cursor-pointer select-none bg-primary-400 dark:bg-darkPrimary-500" @click="changePage('/user/animallist')">
+          <button role="link" class="relative after:absolute after:bottom-0 after:left-0 after:right-0 after:h-[2px] after:w-full after:scale-x-0 hover:after:scale-x-100 after:bg-darkPrimary-50 after:transition-transform after:duration-300">動物列表</button>
         </li>
-        <li class="relative z-10 flex items-center justify-center h-10 transition-colors cursor-pointer select-none lg:z-0 bg-primary-400 dark:bg-darkPrimary-500" @click="handleClick">
-          <button role="link" class="relative after:absolute after:bottom-0 after:left-0 after:right-0 after:h-[2px] after:w-full after:origin-bottom after:scale-x-0 after:bg-darkPrimary-50 after:transition-transform after:duration-300 after:ease-[cubic-bezier(0.65_0.05_0.36_1)] hover:after:origin-bottom hover:after:scale-x-100"><i v-show="!this.isDark" class="fa-regular fa-sun fa-fw"></i><i v-show="this.isDark" class="fa-regular fa-moon fa-fw"></i></button>
+        <li class="relative z-10 flex items-center justify-center h-10 cursor-pointer select-none bg-primary-400 dark:bg-darkPrimary-500" @click="handleClick">
+          <button role="link" class="relative after:absolute after:bottom-0 after:left-0 after:right-0 after:h-[2px] after:w-full after:scale-x-0 hover:after:scale-x-100 after:bg-darkPrimary-50 after:transition-transform after:duration-300">
+            <i v-show="!isDark" class="fa-regular fa-sun fa-fw"></i>
+            <i v-show="isDark" class="fa-regular fa-moon fa-fw"></i>
+          </button>
         </li>
-        <li v-if="user.isLogin" class="relative z-10 flex items-center justify-center h-10 transition-colors cursor-pointer select-none lg:z-0 bg-primary-400 dark:bg-darkPrimary-500" @click="logOut">
-          <button role="link" class="relative after:absolute after:bottom-0 after:left-0 after:right-0 after:h-[2px] after:w-full after:origin-bottom after:scale-x-0 after:bg-darkPrimary-50 after:transition-transform after:duration-300 after:ease-[cubic-bezier(0.65_0.05_0.36_1)] hover:after:origin-bottom hover:after:scale-x-100">登出</button>
+        <li v-if="user.isLogin" class="relative z-10 flex items-center justify-center h-10 cursor-pointer select-none bg-primary-400 dark:bg-darkPrimary-500" @click="logOut">
+          <button role="link" class="relative after:absolute after:bottom-0 after:left-0 after:right-0 after:h-[2px] after:w-full after:scale-x-0 hover:after:scale-x-100 after:bg-darkPrimary-50 after:transition-transform after:duration-300">登出</button>
         </li>
-        <li v-if="!user.isLogin" class="relative z-10 flex items-center justify-center h-10 transition-colors cursor-pointer select-none lg:z-0 bg-primary-400 dark:bg-darkPrimary-500" @click="chagnePage('/login')">
-          <button role="link" class="relative after:absolute after:bottom-0 after:left-0 after:right-0 after:h-[2px] after:w-full after:origin-bottom after:scale-x-0 after:bg-darkPrimary-50 after:transition-transform after:duration-300 after:ease-[cubic-bezier(0.65_0.05_0.36_1)] hover:after:origin-bottom hover:after:scale-x-100">立即登入</button>
+        <li v-if="!user.isLogin" class="relative z-10 flex items-center justify-center h-10 cursor-pointer select-none bg-primary-400 dark:bg-darkPrimary-500" @click="changePage('/login')">
+          <button role="link" class="relative after:absolute after:bottom-0 after:left-0 after:right-0 after:h-[2px] after:w-full after:scale-x-0 hover:after:scale-x-100 after:bg-darkPrimary-50 after:transition-transform after:duration-300">立即登入</button>
         </li>
       </ul>
     </div>
